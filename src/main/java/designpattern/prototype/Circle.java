dc1f7b7f48cec69bef7073f2c2d62944e82f200d
tony <chcjswo@gmail.com>
2021-05-29 00:53:02 +0900

    Prototype Pattern

---
+++ src/main/java/designpattern/prototype/Circle.java
@@ -0,0 +1,57 @@
+package designpattern.prototype;
+
+
+/**
+ * @author chcjswo
+ * @version 1.0.0
+ * @blog https://mocadev.tistory.com
+ * @github https://github.com/chcjswo
+ * @since 2021-05-29
+ **/
+public class Circle extends Shape {
+
+    private int x, y, r;
+
+    public Circle(int x, int y, int r) {
+        this.x = x;
+        this.y = y;
+        this.r = r;
+    }
+
+    public Circle copy() throws CloneNotSupportedException {
+        Circle circle = (Circle) clone();
+        circle.x++;
+        circle.y++;
+        circle.r++;
+        return circle;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public int getR() {
+        return r;
+    }
+
+    public void setR(int r) {
+        this.r = r;
+    }
+
+    @Override
+    public String toString() {
+        return "Circle{" + "x=" + x + ", y=" + y + ", r=" + r + '}';
+    }
+}
