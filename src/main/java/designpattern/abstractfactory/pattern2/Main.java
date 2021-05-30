c0f249ef5a24edfcfae581b27124c090481b7326
tony <chcjswo@gmail.com>
2021-05-31 01:06:14 +0900

    Abstract Factory Pattern 2

--- src/main/java/designpattern/abstractfactory/pattern2/Main.java
+++ src/main/java/designpattern/abstractfactory/pattern2/Main.java
@@ -1,10 +1,9 @@
 package designpattern.abstractfactory.pattern2;
 
+import designpattern.abstractfactory.pattern2.concrete.FactoryInstance;
 import designpattern.abstractfactory.pattern2.factory.Button;
 import designpattern.abstractfactory.pattern2.factory.GuiFactory;
 import designpattern.abstractfactory.pattern2.factory.Textarea;
-import designpattern.abstractfactory.pattern2.linux.LinuxGuiFactory;
-import designpattern.abstractfactory.pattern2.mac.MacGuiFactory;
 
 /**
  * @author chcjswo
@@ -16,22 +15,12 @@ import designpattern.abstractfactory.pattern2.mac.MacGuiFactory;
 public class Main {
 
     public static void main(String[] args) {
-        GuiFactory factory = new LinuxGuiFactory();
+        GuiFactory factory = FactoryInstance.getFactory();
         Button button = factory.createButton();
         Textarea textarea = factory.createTextarea();
 
         button.click();
         System.out.println("textarea.getText() = " + textarea.getText());
-
-        GuiFactory macGuiFactory = new MacGuiFactory();
-        Button macButton = macGuiFactory.createButton();
-        Textarea macTextarea = macGuiFactory.createTextarea();
-
-        macButton.click();
-        System.out.println("macTextarea.getText() = " + macTextarea.getText());
-
-
-
     }
 
 }
--- src/main/java/designpattern/abstractfactory/pattern2/concrete/FactoryInstance.java
+++ src/main/java/designpattern/abstractfactory/pattern2/concrete/FactoryInstance.java
@@ -1,9 +1,8 @@
 package designpattern.abstractfactory.pattern2.concrete;
 
+import designpattern.abstractfactory.pattern2.factory.Button;
 import designpattern.abstractfactory.pattern2.factory.GuiFactory;
-import designpattern.abstractfactory.pattern2.linux.LinuxGuiFactory;
-import designpattern.abstractfactory.pattern2.mac.MacGuiFactory;
-import designpattern.abstractfactory.pattern2.win.WinGuiFactory;
+import designpattern.abstractfactory.pattern2.factory.Textarea;
 
 /**
  * @author chcjswo
@@ -28,3 +27,82 @@ public class FactoryInstance {
     }
 
 }
+
+class LinuxGuiFactory implements GuiFactory {
+    @Override
+    public Button createButton() {
+        return new LinuxButton();
+    }
+
+    @Override
+    public Textarea createTextarea() {
+        return new LinuxTextarea();
+    }
+}
+
+class LinuxButton implements Button {
+    @Override
+    public void click() {
+        System.out.println("Linux Button Click");
+    }
+}
+
+class LinuxTextarea implements Textarea {
+    @Override
+    public String getText() {
+        return "Linux Textarea";
+    }
+}
+
+class MacGuiFactory implements GuiFactory {
+    @Override
+    public Button createButton() {
+        return new MacButton();
+    }
+
+    @Override
+    public Textarea createTextarea() {
+        return new MacTextarea();
+    }
+}
+
+class MacButton implements Button {
+    @Override
+    public void click() {
+        System.out.println("Mac Button Click");
+    }
+}
+
+class MacTextarea implements Textarea {
+    @Override
+    public String getText() {
+        return "Mac Textarea";
+    }
+}
+
+class WinGuiFactory implements GuiFactory {
+    @Override
+    public Button createButton() {
+        return new WinButton();
+    }
+
+    @Override
+    public Textarea createTextarea() {
+        return new WinTextarea();
+    }
+}
+
+class WinButton implements Button {
+    @Override
+    public void click() {
+        System.out.println("Win Button Click");
+    }
+}
+
+class WinTextarea implements Textarea {
+    @Override
+    public String getText() {
+        return "Win Textarea";
+    }
+}
+
